8899882565ede024f32c0f1c2c2f473929cba41f
Michael Myung <michael.h.myung@gmail.com>
2017-11-10 19:57:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ function ternaryCheckCity(city) {
 
 function switchOnCharmFromTip(tip) {
   switch (tip) {
-    case tip = "generous":
+    case "generous":
       return "Thank you so much."
       break
-    case tip = "not as generous":
+    case "not as generous":
       return "Thank you."
       break
     default:
