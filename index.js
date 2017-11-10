46295eb7f81f8ae66b1ff416b1c535b55a1fc2d1
Michael Myung <michael.h.myung@gmail.com>
2017-11-10 19:32:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function scuberGreetingForFeet(distance){
 }
 
 function ternaryCheckCity(city) {
-  city='NYC' ? "Ok, sounds good.": "No go."
+  city='NYC' ? console.log("Ok, sounds good."): console.log("No go.")
 }
